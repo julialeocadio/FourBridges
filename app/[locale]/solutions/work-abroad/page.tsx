2ff3066d6b10c897f30f224210bbf2dccca6d622
julia <leocadiojulia@gmail.com>
2026-08-05 21:32:06 +0200

    modified:   app/[locale]/solutions/work-abroad/page.tsx 	modified:   components/solutions/work/WorkHero.tsx 	modified:   messages/en/solutions.json 	modified:   messages/es/solutions.json 	modified:   messages/pt/solutions.json

--- app/[locale]/solutions/work-abroad/page.tsx
+++ app/[locale]/solutions/work-abroad/page.tsx
@@ -1,8 +1,10 @@
+import WorkHero from "@/components/solutions/work/WorkHero";
 import WorkCTA from "@/components/solutions/work/WorkCTA";
 
 export default function WorkAbroadPage() {
     return (
         <>
+        <WorkHero />
         <WorkCTA />
         </>
     )
