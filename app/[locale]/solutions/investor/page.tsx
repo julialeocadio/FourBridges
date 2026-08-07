1711ce56168e0fd8f6f6675c587bdc903cad6322
julia <leocadiojulia@gmail.com>
2026-08-07 13:20:39 +0200

    modified:   app/[locale]/solutions/investor/page.tsx 	modified:   components/solutions/investor/InvestorHero.tsx 	modified:   messages/en/solutions.json 	modified:   messages/es/solutions.json 	modified:   messages/pt/solutions.json

--- app/[locale]/solutions/investor/page.tsx
+++ app/[locale]/solutions/investor/page.tsx
@@ -1,8 +1,10 @@
+import InvestorHero from "@/components/solutions/investor/InvestorHero";
 import InvestorCTA from "@/components/solutions/investor/InvestorCTA";
 
 export default function InvestorPage() {
     return (
         <>
+            <InvestorHero />
             <InvestorCTA />
         </>
     )
