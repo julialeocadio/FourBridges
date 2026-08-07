78a75e4309fa931108307c2a3646e91a65d8dea8
julia <leocadiojulia@gmail.com>
2026-08-07 09:58:20 +0200

    new file:   app/[locale]/solutions/investor/page.tsx 	modified:   components/solutions/investor/InvestorCTA.tsx 	modified:   messages/en/solutions.json 	modified:   messages/es/solutions.json 	modified:   messages/pt/solutions.json

---
+++ app/[locale]/solutions/investor/page.tsx
@@ -0,0 +1,9 @@
+import InvestorCTA from "@/components/solutions/investor/InvestorCTA";
+
+export default function InvestorPage() {
+    return (
+        <>
+            <InvestorCTA />
+        </>
+    )
+}
