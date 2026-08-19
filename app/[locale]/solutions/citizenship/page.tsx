0cb932d6e73f09f39e1b83bd2a7ef086dbc20bec
julia <leocadiojulia@gmail.com>
2026-08-19 20:35:36 +0200

    modified:   app/[locale]/solutions/citizenship/page.tsx 	modified:   components/solutions/citizenship/CitizenshipSupport.tsx 	modified:   messages/en/solutions.json 	modified:   messages/es/solutions.json 	modified:   messages/pt/solutions.json

--- app/[locale]/solutions/citizenship/page.tsx
+++ app/[locale]/solutions/citizenship/page.tsx
@@ -1,10 +1,12 @@
 import CitizenshipHero from "@/components/solutions/citizenship/CitizenshipHero";
+import CitizenshipServices from "@/components/solutions/citizenship/CitizenshipSupport";
 import CitizenshipCTA from "@/components/solutions/citizenship/CitizenshipCTA";
 
 export default function CitizenshipPage () {
     return (
         <>
         <CitizenshipHero />
+        <CitizenshipServices />
         <CitizenshipCTA />
         </>
     );
