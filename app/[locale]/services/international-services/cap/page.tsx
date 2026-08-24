55c2fafcb64f27c990f389091bf3719fd0c7cf42
julia <leocadiojulia@gmail.com>
2026-08-24 20:58:55 +0200

    modified:   app/[locale]/services/international-services/cap/page.tsx 	modified:   messages/en/cap.json 	modified:   messages/es/cap.json 	modified:   messages/pt/cap.json

--- app/[locale]/services/international-services/cap/page.tsx
+++ app/[locale]/services/international-services/cap/page.tsx
@@ -1,11 +1,13 @@
 import CAPHero from "@/components/sections/services/cap/capHero";
 import CAPAssistance from "@/components/sections/services/cap/capAssistance";
+import CAPCTA from "@/components/sections/services/cap/capCTA";
 
 export default function CAPPage(){
     return(
         <>
         <CAPHero />
         <CAPAssistance />
+        <CAPCTA />
         </>
     )
 }
