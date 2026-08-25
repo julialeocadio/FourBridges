8b860688e5da45bb80d0f4c02dd29347b9e3379b
julia <leocadiojulia@gmail.com>
2026-08-25 20:24:47 +0200

    modified:   app/[locale]/services/international-services/foreign-judgment-validation/page.tsx 	modified:   components/sections/services/foreignJudgment/ForeignJudgmentCTA.tsx 	modified:   messages/en/foreignJudgment.json 	modified:   messages/es/foreignJudgment.json 	modified:   messages/pt/foreignJudgment.json

--- app/[locale]/services/international-services/foreign-judgment-validation/page.tsx
+++ app/[locale]/services/international-services/foreign-judgment-validation/page.tsx
@@ -1,11 +1,13 @@
 import ForeignJudgmentHero from "@/components/sections/services/foreignJudgment/ForeignJudgmentHero";
 import ForeignJudgmentAssistance from "@/components/sections/services/foreignJudgment/foreignJudgmentassistance";
+import ForeignJudgementCTA from "@/components/sections/services/foreignJudgment/ForeignJudgmentCTA";
 
 export default function(){
     return(
         <>
         <ForeignJudgmentHero />
         <ForeignJudgmentAssistance />
+        <ForeignJudgementCTA />
         </>
     )
 }
