50994aafb17ecceac091ed8a42448a8da6494562
julia <leocadiojulia@gmail.com>
2026-08-25 12:25:56 +0200

    modified:   app/[locale]/services/international-services/criminal-record-certificate/page.tsx 	modified:   components/sections/services/criminalRecordCertificate/criminalRecordsCTA.tsx 	modified:   messages/en/criminalRecordCertificate.json 	modified:   messages/es/criminalRecordCertificate.json 	modified:   messages/pt/criminalRecordCertificate.json

--- app/[locale]/services/international-services/criminal-record-certificate/page.tsx
+++ app/[locale]/services/international-services/criminal-record-certificate/page.tsx
@@ -1,11 +1,13 @@
 import CriminalRecordHero from "@/components/sections/services/criminalRecordCertificate/criminalRecordsHero";
 import CriminalRecordAssistance from "@/components/sections/services/criminalRecordCertificate/criminalRecordAssistance";
+import CriminalRecordCTA from "@/components/sections/services/criminalRecordCertificate/criminalRecordsCTA";
 
 export default function CriminalRecordCertificatePage(){
     return(
         <>
         <CriminalRecordHero />
         <CriminalRecordAssistance />
+        <CriminalRecordCTA />
         </>
     )
 }
