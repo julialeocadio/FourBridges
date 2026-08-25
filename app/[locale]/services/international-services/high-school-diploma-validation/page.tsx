ca052c49f01fb63cc54c335e4ca95cbaa18e4321
julia <leocadiojulia@gmail.com>
2026-08-25 22:13:24 +0200

    modified:   app/[locale]/services/international-services/high-school-diploma-validation/page.tsx 	new file:   components/sections/services/diplomaRecognition/HighSchoolDiplomaCTA.tsx 	deleted:    components/sections/services/diplomaRecognition/diplomaCTA.tsx 	modified:   messages/en/diplomaRecognition.json 	modified:   messages/es/diplomaRecognition.json 	modified:   messages/pt/diplomaRecognition.json

--- app/[locale]/services/international-services/high-school-diploma-validation/page.tsx
+++ app/[locale]/services/international-services/high-school-diploma-validation/page.tsx
@@ -1,11 +1,13 @@
 import HighSchoolDiplomaHero from "@/components/sections/services/diplomaRecognition/HighSchoolDiplomeHero";
 import HighSchoolDiplomaAssistance from "@/components/sections/services/diplomaRecognition/HighSchoolDiplomaAssistance";
+import HighSchoolDiplomaCTA from "@/components/sections/services/diplomaRecognition/HighSchoolDiplomaCTA";
 
 export default function(){
     return(
         <>
         <HighSchoolDiplomaHero />
         <HighSchoolDiplomaAssistance />
+        <HighSchoolDiplomaCTA />
         </>
     )
 }
