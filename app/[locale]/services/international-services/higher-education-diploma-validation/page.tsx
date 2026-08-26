cb8867a77b4da693aefbd6b102932f712294eb8b
julia <leocadiojulia@gmail.com>
2026-08-26 11:25:10 +0200

    modified:   app/[locale]/services/international-services/higher-education-diploma-validation/page.tsx 	modified:   components/sections/services/diplomaRecognition/HigherEducationDiplomaAssistance.tsx 	modified:   messages/en/diplomaRecognition.json 	modified:   messages/es/diplomaRecognition.json 	modified:   messages/pt/diplomaRecognition.json

--- app/[locale]/services/international-services/higher-education-diploma-validation/page.tsx
+++ app/[locale]/services/international-services/higher-education-diploma-validation/page.tsx
@@ -1,9 +1,11 @@
 import HigherEducationHero from "@/components/sections/services/diplomaRecognition/HigherEducationDiplomaHero";
+import HigherEducationAssistance from "@/components/sections/services/diplomaRecognition/HigherEducationDiplomaAssistance";
 
 export default function(){
     return(
         <>
         <HigherEducationHero />
+        <HigherEducationAssistance />
         </>
     )
 }
