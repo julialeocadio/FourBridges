e977def7c65f37677b7b4bb0963f157e16681903
julia <leocadiojulia@gmail.com>
2026-09-04 22:35:28 +0200

    modified:   app/[locale]/services/international-services/crnm-rnm-rne/page.tsx 	modified:   components/sections/services/crnm/crnmCTA.tsx

--- app/[locale]/services/international-services/crnm-rnm-rne/page.tsx
+++ app/[locale]/services/international-services/crnm-rnm-rne/page.tsx
@@ -1,11 +1,13 @@
 import CRNMHero from "@/components/sections/services/crnm/crnmHero";
 import CRNMHelps from "@/components/sections/services/crnm/crnmHelps";
+import CRNMCTA from "@/components/sections/services/crnm/crnmCTA";
 
 export default function CRNMPage(){
     return(
         <>
         <CRNMHero />
         <CRNMHelps />
+        <CRNMCTA />
         </>
     )
 }
