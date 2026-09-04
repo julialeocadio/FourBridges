7669058093b7b729ff599b17284388be592e726b
julia <leocadiojulia@gmail.com>
2026-09-04 17:52:17 +0200

    modified:   app/[locale]/services/international-services/nie-spain/page.tsx 	modified:   components/sections/services/nieSpain/NieHelps.tsx

--- app/[locale]/services/international-services/nie-spain/page.tsx
+++ app/[locale]/services/international-services/nie-spain/page.tsx
@@ -1,9 +1,11 @@
 import NieHero from "@/components/sections/services/nieSpain/NieHero";
+import NieHelps from "@/components/sections/services/nieSpain/NieHelps";
 
 export default function NiePage() {
     return(
         <>
         <NieHero />
+        <NieHelps />
         </>
     )
 }
