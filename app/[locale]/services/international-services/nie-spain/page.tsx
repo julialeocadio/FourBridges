f1118d5bfb36d2d46269a6ece1fab5cfd7ca9ff6
julia <leocadiojulia@gmail.com>
2026-09-04 17:58:49 +0200

    modified:   app/[locale]/services/international-services/nie-spain/page.tsx 	modified:   components/sections/services/nieSpain/NieCTA.tsx

--- app/[locale]/services/international-services/nie-spain/page.tsx
+++ app/[locale]/services/international-services/nie-spain/page.tsx
@@ -1,11 +1,13 @@
 import NieHero from "@/components/sections/services/nieSpain/NieHero";
 import NieHelps from "@/components/sections/services/nieSpain/NieHelps";
+import NieCTA from "@/components/sections/services/nieSpain/NieCTA";
 
 export default function NiePage() {
     return(
         <>
         <NieHero />
         <NieHelps />
+        <NieCTA />
         </>
     )
 }
