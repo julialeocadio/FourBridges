5aab19959253a8eda1d30a118862aecd6942c22b
julia <leocadiojulia@gmail.com>
2026-09-04 18:33:33 +0200

    modified:   app/[locale]/services/international-services/european-certificates-search/page.tsx 	modified:   components/sections/services/europeanCertificateSearch/europeanCertificateCTA.tsx

--- app/[locale]/services/international-services/european-certificates-search/page.tsx
+++ app/[locale]/services/international-services/european-certificates-search/page.tsx
@@ -1,11 +1,13 @@
 import EuropeanCertificateSearchHero from "@/components/sections/services/europeanCertificateSearch/europeanCertificateHero";
 import EuropeanCertificateSearchHelps from "@/components/sections/services/europeanCertificateSearch/europeanCertificateHelps";
+import EuropeanCertificateSearchCTA from "@/components/sections/services/europeanCertificateSearch/europeanCertificateCTA";
 
 export default function EuropeanCertificateSearchPage(){
     return(
         <>
         <EuropeanCertificateSearchHero />
         <EuropeanCertificateSearchHelps />
+        <EuropeanCertificateSearchCTA />
         </>
     )
 }
