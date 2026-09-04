a941c8af4cfc29e0d140f70aa726b610548bc9e7
julia <leocadiojulia@gmail.com>
2026-09-04 19:09:41 +0200

    modified:   app/[locale]/services/international-services/european-ancestry-search/page.tsx 	modified:   components/sections/services/europeanAncestrySearch/europeanAncestryHelps.tsx

--- app/[locale]/services/international-services/european-ancestry-search/page.tsx
+++ app/[locale]/services/international-services/european-ancestry-search/page.tsx
@@ -1,9 +1,11 @@
 import EuropeanAncestrySearchHero from "@/components/sections/services/europeanAncestrySearch/europeanAncestryHero";
+import EuropeanAncestrySearchHelps from "@/components/sections/services/europeanAncestrySearch/europeanAncestryHelps";
 
 export default function EuropeanAncestrySearchPage(){
     return(
         <>
         <EuropeanAncestrySearchHero />
+        <EuropeanAncestrySearchHelps />
         </>
     )
 }
