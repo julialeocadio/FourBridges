31921acd81f5ff782c6d62424ad3a06fd4acea3d
julia <leocadiojulia@gmail.com>
2026-09-04 19:12:28 +0200

    modified:   app/[locale]/services/international-services/european-ancestry-search/page.tsx 	modified:   components/sections/services/europeanAncestrySearch/europeanAncestryCTA.tsx

--- app/[locale]/services/international-services/european-ancestry-search/page.tsx
+++ app/[locale]/services/international-services/european-ancestry-search/page.tsx
@@ -1,11 +1,13 @@
 import EuropeanAncestrySearchHero from "@/components/sections/services/europeanAncestrySearch/europeanAncestryHero";
 import EuropeanAncestrySearchHelps from "@/components/sections/services/europeanAncestrySearch/europeanAncestryHelps";
+import EuropeanAncestrySearchCTA from "@/components/sections/services/europeanAncestrySearch/europeanAncestryCTA";
 
 export default function EuropeanAncestrySearchPage(){
     return(
         <>
         <EuropeanAncestrySearchHero />
         <EuropeanAncestrySearchHelps />
+        <EuropeanAncestrySearchCTA />
         </>
     )
 }
